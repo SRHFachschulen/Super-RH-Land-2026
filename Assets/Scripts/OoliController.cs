a72af7511966cc22f948f92f76f7668575ef591b
Oliver Pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2026-08-24 12:25:57 +0200

    Jumping

--- Assets/Scripts/OoliController.cs
+++ Assets/Scripts/OoliController.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class OoliController : MonoBehaviour
-{
+public class OoliController : MonoBehaviour{
     public float speed = 5;
+        
     public CharacterController cc;
+    
+    public float verticalVelocity = 0;
+    public float gravity = 9.81f;
+    public float jumpPower = 20;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
@@ -14,9 +18,25 @@ public class OoliController : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
+        // Horizontal Movement (Absolute)
         Vector2 input = InputSystem.actions.FindAction("Move").ReadValue<Vector2>();
         Vector3 movement = new Vector3(input.x, 0, input.y);
         movement *= Time.deltaTime * speed;
+        
+        // Vertical Movement (Quasi Simulation)
+        if (cc.isGrounded && verticalVelocity < 0){
+            verticalVelocity = 0;
+        }
+        
+        verticalVelocity -= gravity * Time.deltaTime;
+        
+        bool jumpPressed = InputSystem.actions.FindAction("Jump").WasPressedThisFrame();
+        if (jumpPressed && cc.isGrounded){
+            verticalVelocity += jumpPower;
+        }
+
+        movement.y = verticalVelocity;
+        
         cc.Move(movement);
     }
 }
