6bf60a060201ea0408a8a8074c7c93974d911ed2
Oliver Pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2026-08-26 12:20:52 +0200

    Coin pt2 feat. particles

--- Assets/Scripts/CollectibleCoin.cs
+++ Assets/Scripts/CollectibleCoin.cs
@@ -1,16 +1,25 @@
+using System;
 using UnityEngine;
 
-public class CollectibleCoin : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+public class CollectibleCoin : MonoBehaviour{
+    [SerializeField]
+    private float rotationSpeed = 90;
+    [SerializeField]
+    private GameObject OnCollectionFX;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        var oc = other.GetComponent<OoliController>();
+        if (oc == null) return;
+        if (OnCollectionFX != null){
+            Instantiate(OnCollectionFX, transform.position, Quaternion.identity);
+        }
+        // Todo call event
+        Destroy(gameObject);
     }
 }
