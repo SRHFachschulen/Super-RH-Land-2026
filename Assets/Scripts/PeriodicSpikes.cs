28f9f7df3012ac33fdc13c3cf74ce3a3c7b1162b
Oliver Pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2026-08-26 12:56:21 +0200

    Periodic Spikes Improvements

--- Assets/Scripts/PeriodicSpikes.cs
+++ Assets/Scripts/PeriodicSpikes.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PeriodicSpikes : MonoBehaviour{
     private float upY = 0;
     private float downY = -0.2f;
+    private float spikeSpeed = 3;
 
     public Transform theSpikes;
 
@@ -15,7 +16,9 @@ public class PeriodicSpikes : MonoBehaviour{
     void Update(){
         float sinVal = Mathf.Sin(phaseShift + (Time.time * 2 * Mathf.PI * frequency));
         var childLocalPosition = theSpikes.localPosition;
-        childLocalPosition.y = sinVal > 0 ? upY : downY;
+        childLocalPosition.y += spikeSpeed * Time.deltaTime * (sinVal > 0 ? 1 : -1);
+        childLocalPosition.y = Mathf.Clamp(childLocalPosition.y, downY, upY);
+        theSpikes.GetComponent<Collider>().enabled = !(childLocalPosition.y <= (downY+upY/2));
         theSpikes.localPosition = childLocalPosition;
     }
 }
